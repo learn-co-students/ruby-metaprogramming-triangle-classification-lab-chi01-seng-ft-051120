ed5ec1e784a39eeecb34fdd48fe840f1b1196ba9
okaydokay97 <rubens.fernandes97@gmail.com>
2020-05-22 20:27:52 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+attr_reader :side_one, :side_two, :side_three
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+    def kind
+      if side_one == 0 || side_two == 0 || side_three == 0
+        raise TriangleError
+      elsif side_one.negative? || side_two.negative? || side_three.negative?
+        raise TriangleError
+      elsif side_one + side_two <= side_three || side_two + side_three <= side_one || side_one + side_three <= side_two
+        raise TriangleError
+      elsif side_one == side_two && side_one == side_three
+        return :equilateral
+      elsif side_one != side_three && side_one != side_two && side_two != side_three
+        return :scalene 
+      else 
+        return :isosceles
+      end
+    end
+
+
+    class TriangleError < StandardError
+      def message
+        puts "Please enter valid side lengths."
+      end
+    end
 end
